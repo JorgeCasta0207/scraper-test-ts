695dcaea512e2b22411c40a3eee0e6747847ddd2
Jorge Casta <jorgerochin0102@gmail.com>
2023-10-25 16:09:47 -0700

    Testing News Sites to see which may work

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,22 @@
-// import puppeteer from 'puppeteer';
-// async function takeScreenshot() {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('https://bot.sannysoft.com/');
-//   await page.screenshot({ path: 'example.png' });
-//   await browser.close();
-// }
-// takeScreenshot();
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-puppeteer.launch({ headless: true }).then(async (browser) => {
+const navigationTimeout = 60000;
+puppeteer.launch({ headless: false }).then(async (browser) => {
     console.log('Running Tests....');
     const page = await browser.newPage();
-    await page.goto('https://bot.sannysoft.com/');
-    await page.waitForTimeout(5000);
-    await page.screenshot({ path: 'testresult.png', fullPage: true });
-    await browser.close();
-    console.log(`All done, check the screenshot. ✨`);
+    const url = 'https://www.kcra.com/';
+    try {
+        await page.goto(url, { waitUntil: 'load', timeout: 0 });
+        await page.waitForTimeout(5000);
+        await page.screenshot({ path: 'testresult.png', fullPage: true });
+        console.log(`Screenshot captured right away pimp.✨`);
+    }
+    catch (error) {
+        console.log('Navigation failed pimp:', error);
+    }
+    finally {
+        await browser.close();
+        console.log(`All done...`);
+    }
 });
