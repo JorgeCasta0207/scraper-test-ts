58085865ad7182d081370a933be331b33329d350
Jorge Casta <jorgerochin0102@gmail.com>
2023-10-30 15:56:56 -0700

    Testing screenshot on Youtube and it's searchbar

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,14 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-const navigationTimeout = 60000;
-puppeteer.launch({ headless: false }).then(async (browser) => {
-    console.log('Running Tests....');
+const url = 'https://www.youtube.com/';
+const searchTermCLI = process.argv.length > 2 ? process.argv[2] : 'Nissan GTR R35';
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    const url = 'https://www.kcra.com/';
-    try {
-        await page.goto(url, { waitUntil: 'load', timeout: 0 });
-        await page.waitForTimeout(5000);
-        await page.screenshot({ path: 'testresult.png', fullPage: true });
-        console.log(`Screenshot captured right away pimp.✨`);
-    }
-    catch (error) {
-        console.log('Navigation failed pimp:', error);
-    }
-    finally {
-        await browser.close();
-        console.log(`All done...`);
-    }
-});
+    await page.goto(url);
+    await page.waitForSelector('#search-input #search');
+    await page.type('#search-input #search', searchTermCLI, { delay: 200 });
+    await page.screenshot({ path: './youtube-test.png' });
+    await browser.close();
+})();
